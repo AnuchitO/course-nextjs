5c84a0b3328731c6852753e030b4699875c72ca3
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2026-06-30 16:03:38 +0000

    FlightCard refactor: promote originCode to a prop

--- 03-flight-card/src/FlightCard.tsx
+++ 03-flight-card/src/FlightCard.tsx
@@ -1,5 +1,4 @@
-function FlightCard({ takeoffIcon, airline, flightNumber, price, priceSuffix, departureTime }) {
-  const originCode = "BKK"
+function FlightCard({ takeoffIcon, airline, flightNumber, price, priceSuffix, departureTime, originCode }) {
   const duration = "2h 30m"
   const routeIcon = "flight"
   const stopLabel = "Non-stop"
