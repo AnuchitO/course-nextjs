3d2e533fa7a89751fd242cecd225b5293586ed2a
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2026-06-30 15:59:06 +0000

    FlightList structure: wrap cards in vertical list container

--- 03-flight-card/src/FlightList.tsx
+++ 03-flight-card/src/FlightList.tsx
@@ -2,13 +2,13 @@ import FlightCard from './FlightCard'
 
 function FlightList() {
   return (
-    <>
+    <div className="flex flex-col gap-md">
       <FlightCard />
       <FlightCard />
       <FlightCard />
       <FlightCard />
       <FlightCard />
-    </>
+    </div>
   )
 }
 
